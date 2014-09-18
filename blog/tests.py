75cd0f9fa5090ba4c8444758d0a891f8005d24c4
Salil <salil@dfordata.com>
2014-09-18 17:27:04 +0530

    Added tests(currently testing for url mapping only) in tests.py

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+from django.contrib.auth.models import User
+
+class BlogTests( TestCase ):
+
+    def test_urls(self):
+        #Testing status_code for urls.
+        c = Client()
+        #Testing for get request
+        response = c.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+        response = c.get('/blog/contact')
+        self.assertEqual(response.status_code, 200)
+        response = c.get('/blog/add')
+        self.assertEqual(response.status_code, 302)
+        response = c.get('/blog/login')
+        self.assertEqual(response.status_code, 200)
+        response = c.get('/blog/post/')
+        self.assertEqual(response.status_code, 404)
+        
+        #Creating a User object to test login process
+        user = User.objects.create(username='dummy')
+        user.set_password('newpassword')
+        user.save()
+        #Testing post request for login page
+        response = c.post('/blog/login', \
+                    {'username':'dummy', 'password':'newpassword'})
+        self.assertEqual(response.status_code, 302)
+        response = c.get('/blog/add')
+        self.assertEqual(response.status_code, 200)
+        
